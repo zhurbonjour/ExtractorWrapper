f893460eda53797c7e6fad9033e9ed424d599b69
zhurbonjour <alexzhuravle@gmail.com>
2022-10-29 18:40:05 +0300

    executable for linux

--- extractor_wrapper.py
+++ extractor_wrapper.py
@@ -1,7 +1,6 @@
-#!/home/alexeyz/PycharmProjects/ExtractorWrapper/venv/bin python3
+#!/usr/bin/env python3
 from application_window import extractor_wrapper
 
 
 if __name__ == '__main__':
     extractor_wrapper()
-
